55976f4216755418c0fd0f28dda15613d24701cb
Kalpesh Parashar <174053987+Kalpesh-ops@users.noreply.github.com>
2026-02-10 17:04:11 +0530

    Fix: Correct image file extension for vaibhavi in Social Media and Content Team

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -34,7 +34,7 @@ export default function Navbar() {
                          </nav>
                     </div>
                     <div className="flex justify-end gap-4">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-menu md:hidden" onClick={()=> setIsOpen(!isOpen)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-menu md:hidden" onClick={()=> setIsOpen(!isOpen)}>
                         <line x1="3" y1="6" x2="21" y2="6" className={twMerge("origin-left transition",isOpen && 'rotate-45 -translate-y-1')}></line>
                         <line x1="3" y1="12" x2="21" y2="12" className={twMerge("transition",isOpen && "opacity-0")}></line>
                         <line x1="3" y1="18" x2="21" y2="18" className={twMerge("origin-left transition",isOpen && '-rotate-45 translate-y-1')}></line>
