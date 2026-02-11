d2e9de20cbe8ebec2d531305469f8617432f4e1f
DevyanshuNegi <devyanshunegi@gmail.com>
2026-02-12 00:53:02 +0530

    build errors fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    // Disable ESLint during builds
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     // Disable TypeScript errors during builds (optional)
     ignoreBuildErrors: true,
